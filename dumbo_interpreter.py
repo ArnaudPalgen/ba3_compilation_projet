e2dde7a6c6549b6663c486f65bfeddd1c521efdd
arnaudPalgen <arnaud.palgen@student.umons.ac.be>
2019-03-26 16:46:08 +0100

    fix some bugs

--- dumbo_interpreter.py
+++ dumbo_interpreter.py
@@ -1,44 +1,77 @@
 import ply.lex as lex
 
-tokens=("START_BLOC", "END_BLOC", "STRING", "TEXT", "INSTRUCTION", "END_EXPRESSION", "ASSIGNATION", "LPARENT", "RPARENT", "PONCTUATION")
+tokens = (
+    "START_BLOC",
+    "END_BLOC",
+    "STRING",
+    "TEXT",
+    "INSTRUCTION",
+    "END_EXPRESSION",
+    "ASSIGNATION",
+    "LPARENT",
+    "RPARENT",
+    "PONCTUATION",
+)
 
-t_START_BLOC=r'\{\{'
+t_ignore = " \t"
 
-t_END_BLOC=r'\}\}'
+t_START_BLOC = r"\{\{"
+
+t_END_BLOC = r"\}\}"
+
 
 def t_INSTRUCTION(t):
-    r'for|in|do|endfor|print'
-    t.value=str(t.value)
+    r"for|in|do|endfor|print"
+    t.value = str(t.value)
     return t
+
 
 def t_PONCTUATION(t):
-    r'\.|,'
-    t.value=str(t.value)
+    r"\.|,"
+    t.value = str(t.value)
     return t
 
-t_ASSIGNATION=r':='
 
-t_END_EXPRESSION=r';'
+def t_ASSIGNATION(t):
+    r":="
+    t.value = str(t.value)
+    return t
 
-t_LPARENT=r'\('
 
-t_RPARENT=r'\)'
+def t_END_EXPRESSION(t):
+    r";"
+    t.Value = str(t.value)
+    return t
+
+
+def t_LPARENT(t):
+    r"\("
+    t.value = str(t.value)
+    return t
+
+
+def t_RPARENT(t):
+    r"\)"
+    t.Value = str(t.value)
+    return t
+
 
 def t_STRING(t):
-    r' \' (\w |;| &| < |> |"| −| \.| \\|\/| \n| \ p| :| , )+ \' '
-    t.value=str(t.value)
+    r"\'(\w|;|&|<|>|\"|_|-|\.|\\|\/|\\n|:|,|\ )+\'"
+    t.value = str(t.value)
     return t
 
+
 def t_TEXT(t):
-    r'(\w |;| &| < | > |"| −| \.| \\|\/| \n| \ p| :| , )+'
-    t.value=str(t.value)
+    r"(\w|;|&|<|>|\"|_|-|\.|\\|\/|\\n|:|,)+"
+    t.value = str(t.value)
     return t
+
 
 def t_newline(t):
     r"\n+"
     t.lexer.lineno += len(t.value)
 
-t_ignore = " \t"
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
@@ -47,9 +80,9 @@ def t_error(t):
 
 if __name__ == "__main__":
     import sys
-    
+
     lexer = lex.lex()
 
     lexer.input(sys.stdin.read())
     for token in lexer:
-        print("line %d : %s (%s) " %(token.lineno, token.type, token.value))
+        print("line %d : %s (%s) " % (token.lineno, token.type, token.value))
